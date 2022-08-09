6cd94d7f490148aebe9f67f69a384d0a4fc91dae
Brandon Bashein <b2randon@gmail.com>
2022-08-09 00:11:25 -0600

    add ui header component

---
+++ UI/header/index.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+export default function Header() {
+    return (
+        <div>
+            <div>Home</div>
+            <div>Recipes</div>
+            <div>About</div>
+        </div>
+    );
+}
