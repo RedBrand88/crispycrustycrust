52963267c4fb5d419feeb1b86052d17a28fd6db6
Brandon Bashein <b2randon@gmail.com>
2022-08-09 00:27:29 -0600

    add footer ui component

---
+++ UI/footer/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div>
+            <div>Footer Section</div>
+            <div>Contact Section</div>
+        </div>
+    );
+}
